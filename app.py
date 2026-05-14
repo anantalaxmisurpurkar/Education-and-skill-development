726b31fd504a5d152a7d7461efbc90d34077df3b
anantalaxmisurpurkar <anantalaxmisurpurkar@gmail.com>
2026-05-14 15:19:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "secretkey"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+# -----------------------------
+# DATABASE MODEL
+# -----------------------------
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+# -----------------------------
+# HOME PAGE
+# -----------------------------
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# -----------------------------
+# REGISTER
+# -----------------------------
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect('/login')
+
+    return render_template('register.html')
+
+# -----------------------------
+# LOGIN
+# -----------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(
+            username=username,
+            password=password
+        ).first()
+
+        if user:
+            session['user'] = username
+            return redirect('/dashboard')
+
+    return render_template('login.html')
+
+# -----------------------------
+# DASHBOARD
+# -----------------------------
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html')
+
+    return redirect('/login')
+
+# -----------------------------
+# QUIZ PAGE
+# -----------------------------
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')
+
+# -----------------------------
+# RUN APP
+# -----------------------------
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
